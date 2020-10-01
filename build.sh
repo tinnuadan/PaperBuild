32ee0b2114f7b2feb94c7756cdd4f5d98453792c
tinnuadan <tinnuadan@gmail.com>
2020-10-02 00:02:32 +0200

    fixed cp

--- build.sh
+++ build.sh
@@ -35,4 +35,5 @@ fi
 workdir="work"
 minecraftversion=$(cat "$workdir/BuildData/info.json"  | grep minecraftVersion | cut -d '"' -f 4)
 date=$(date +"%Y-%m-%d")
-cp "paperclip.jar" "$output/paper_$minecraftversion_$date.jar"
+echo "Copying to \"$output/paper_${minecraftversion}_${date}.jar\""
+cp "paperclip.jar" "$output/paper_${minecraftversion}_${date}.jar"
