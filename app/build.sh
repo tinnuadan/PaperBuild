a119998008dcaa32c76b28933e0add72d41e5f2d
tinnuadan <tinnuadan@gmail.com>
2022-01-30 12:44:27 +0100

    adapted to new paper build system

--- app/build.sh
+++ app/build.sh
@@ -1,11 +1,10 @@
 #/bin/bash
 
-#rootdir=$(pwd)
 rootdir=$(pwd)
 # read config but get rid of windows-style line feeds first
 source <(grep = config.ini | sed 's/\r//g')
 
-
+# Get latest commit of paper
 if $(test -d "Paper"); then
   cd Paper
   git clean -dfx
@@ -24,20 +23,23 @@ fi
 # set java options
 export _JAVA_OPTIONS="$java_options"
 
-# apply fixes first
-git apply "$rootdir/patches/init_sh.patch"
-git apply "$rootdir/patches/build_sh.patch"
+# blacklist patches
+python3 $rootdir/blacklist_patches.py "${rootdir}/blacklist.txt"
 
 echo "Starting build"
-./paper jar
-
+./gradlew applyPatches
+./gradlew createReobfBundlerJar
+ 
 output="$rootdir/build/$branch"
 if $(test ! -d $output);
 then
   mkdir -p "$output";
 fi
 workdir="work"
+paper_out=$rootdir/Paper/build/libs/
+build_file=$(ls $paper_out)
 minecraftversion=$(cat "$workdir/BuildData/info.json"  | grep minecraftVersion | cut -d '"' -f 4)
+spigotversion=$(cat "$workdir/BuildData/info.json"  | grep spigotVersion | cut -d '"' -f 4)
 date=$(date +"%Y-%m-%d")
-echo "Copying to \"$output/paper-${minecraftversion}_${date}.jar\""
-cp "paperclip.jar" "$output/paper-${minecraftversion}_${date}.jar"
+echo "Copying ${build_file} to \"$output/paper-${minecraftversion}_${date}.jar\""
+cp "$paper_out/$build_file" "$output/paper-${minecraftversion}_${date}.jar"
